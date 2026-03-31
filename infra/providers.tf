a1d9514fcc466666c62d4efb1cd33b673f23d0c1
shghildi <79434248+shghildi@users.noreply.github.com>
2026-03-31 12:21:09 +0900

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,24 +1,15 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      # Use a recent 4.x version (the Foundry resources are documented in 4.x)
-      version = ">= 4.54.0"
+      version = "~> 3.100"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = false
-      recover_soft_deleted_key_vaults = false
-    }
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-  storage_use_azuread = true
+  features {}
+  use_oidc = true
 }
