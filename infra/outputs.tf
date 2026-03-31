1aeb6110508fea92ad2dc8cddcebd12769010c34
shghildi <79434248+shghildi@users.noreply.github.com>
2026-03-31 09:58:50 +0900

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,9 @@
 
+output "appinsights_id" {
+  value = azurerm_application_insights.appinsights.id
+}
+
+output "instrumentation_key" {
+  value     = azurerm_application_insights.appinsights.instrumentation_key
+  sensitive = true
+}
