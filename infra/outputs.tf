f1c9becdac804e9fc50811a420c87948f98e1bc2
shghildi <79434248+shghildi@users.noreply.github.com>
2026-03-31 10:51:18 +0900

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,14 @@
-
-output "appinsights_id" {
-  value = azurerm_application_insights.appinsights.id
+output "storage_account_id" {
+  description = "Resource ID of the storage account"
+  value       = azurerm_storage_account.storage.id
 }
 
-output "instrumentation_key" {
-  value     = azurerm_application_insights.appinsights.instrumentation_key
-  sensitive = true
+output "storage_account_name" {
+  description = "Name of the storage account"
+  value       = azurerm_storage_account.storage.name
+}
+
+output "primary_blob_endpoint" {
+  description = "Primary Blob endpoint URL"
+  value       = azurerm_storage_account.storage.primary_blob_endpoint
 }
