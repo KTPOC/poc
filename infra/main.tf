7b17633c59b7108f3ec37e8d01272796b7e9a78b
shghildi <79434248+shghildi@users.noreply.github.com>
2026-03-31 09:58:15 +0900

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,11 @@
-infra/main.tf
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = var.appinsights_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
