b921a3d9df951f08068ac232c2db907e1639e324
shghildi <79434248+shghildi@users.noreply.github.com>
2026-03-31 10:39:01 +0900

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,9 +3,20 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_application_insights" "appinsights" {
-  name                = var.appinsights_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
+resource "azurerm_storage_account" "storage" {
+  name                     = "stappdemostore01"   # must be globally unique
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  enable_https_traffic_only = true
+  allow_blob_public_access  = true
+  min_tls_version           = "TLS1_2"
+
+  tags = {
+    environment = "dev"
+    owner       = "infra"
+  }
 }
