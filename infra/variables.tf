d01c5b2a1a9992762f518ae276a38e797b3530e8
shghildi <79434248+shghildi@users.noreply.github.com>
2026-03-31 09:57:55 +0900

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,18 @@
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type        = string
+  default     = "rg-monitoring-demo"
+}
+
+variable "appinsights_name" {
+  description = "Application Insights name"
+  type        = string
+  default     = "appi-demo"
+}
