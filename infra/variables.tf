6823f37d57b0044abc148bf31df40cfd85c00ac3
shghildi <79434248+shghildi@users.noreply.github.com>
2026-04-01 10:22:35 +0900

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,19 +5,3 @@ variable "location" {
 variable "resource_group_name" {
   default = "rg-ai-foundry-prod"
 }
-
-variable "ai_hub_name" {
-  default = "ai-hub-prod-demo"
-}
-
-variable "ai_project_name" {
-  default = "ai-project-prod-demo"
-}
-
-variable "openai_name" {
-  default = "openai-prod-demo-12345"
-}
-variable "openai_model_name" {
-  default = "gpt-4.1"
-}
-
